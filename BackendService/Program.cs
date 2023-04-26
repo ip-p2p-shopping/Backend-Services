fef2395bca1efb90d05d483d4813274a0d21f24c
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2023-04-27 00:48:46 +0300

    allow all cors

--- BackendService/Program.cs
+++ BackendService/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 builder.Services.AddSingleton<JwtProvider>();
 
@@ -53,6 +54,13 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
 
 app.UseAuthentication();
 app.UseAuthorization();
