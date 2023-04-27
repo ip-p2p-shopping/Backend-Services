ff80c6a2e57c76692ddf5d262b82ad941acff6b3
alinaduca <94075775+alinaduca@users.noreply.github.com>
2023-04-27 21:37:08 +0300

    Update AccountController.cs

--- BackendService/Controllers/AccountController.cs
+++ BackendService/Controllers/AccountController.cs
@@ -94,10 +94,10 @@ namespace BackendService
             _user.Address = user.Address;
             _user.FirstName = user.FirstName;
             _user.LastName = user.LastName;
-
+            _user.Salt = user.Salt;
             if (!string.IsNullOrEmpty(user.Password))
             {
-                _user.Password = EncryptionHelpers.ComputeHash(user.Password);
+                _user.Password = EncryptionHelpers.ComputeHash(user.Password, user.Salt);
             }
             
             await _dbContext.SaveChangesAsync();
