bdb8256bbf98af6b98794eeb44d55e74a9dd38d1
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2023-05-03 22:56:14 +0300

    add max price

--- BackendService/Data/Models/Product.cs
+++ BackendService/Data/Models/Product.cs
@@ -6,6 +6,7 @@ public class Product{
   public String Name { get; set; } = string.Empty;
   public String Category { get; set; } = string.Empty;
   public int Price { get; set; }
+  public int MaxPrice { get; set; }
   public int Quantity { get; set; }
   public String MeasureUnit { get; set; } = string.Empty;
   public String SellerId { get; set; } = string.Empty;
---
+++ BackendService/Migrations/20230503195604_maxprice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendService.Migrations
+{
+    /// <inheritdoc />
+    public partial class maxprice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxPrice",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxPrice",
+                table: "Products");
+        }
+    }
+}
