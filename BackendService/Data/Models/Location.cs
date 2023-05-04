e274999feacc569b9b577683d558e854c0bfff2f
LDaniel237 <lkingdaniel90@gmail.com>
2023-05-05 02:47:39 +0300

    added db migration for location product map

--- BackendService/Data/Models/Location.cs
+++ BackendService/Data/Models/Location.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
-        public List<LocationProduct> Products { get; set; }
+        public List<LocationProduct> LocationProducts { get; set; } = new List<LocationProduct>();
     }
 }
--- BackendService/Migrations/20230504233713_location_products_table.Designer.cs
+++ BackendService/Migrations/20230504233713_location_products_table.Designer.cs
@@ -3,6 +3,7 @@ using BackendService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackendService.Migrations
 {
     [DbContext(typeof(ShoppingDbContext))]
-    partial class ShoppingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230504233713_location_products_table")]
+    partial class location_products_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
