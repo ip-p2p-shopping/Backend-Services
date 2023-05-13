7c77e3dfcf9ea4b185ec22174928e025dd20864c
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2023-05-13 18:46:09 +0300

    random id

--- BackendService/Data/Models/GhostLocation.cs
+++ BackendService/Data/Models/GhostLocation.cs
@@ -2,6 +2,10 @@ namespace BackendService.Data.Models;
 
 public class GhostLocation
 {
+    public GhostLocation()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
     public string Id { get; set; }
     public int ProductId { get; set; }
     public double Lat { get; set; }
