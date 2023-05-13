5d0fa4ffb0dc81a9a4230edb4c0eb057c9fff6f9
LDaniel237 <lkingdaniel90@gmail.com>
2023-05-13 10:31:04 +0300

    location models added for migration

---
+++ BackendService/Data/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendService.Data.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public List<LocationProduct> LocationProducts { get; set; } = new List<LocationProduct>();
+    }
+}
---
+++ BackendService/Data/Models/LocationProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendService.Data.Models
+{
+    public class LocationProduct
+    {
+        public int ProductId { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+        public Product Product { get; set; }
+    }
+}
