193d9000dd8ae969e1058863f582baadc11437c1
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2023-05-13 17:27:58 +0300

    migrations

---
+++ BackendService/Migrations/20230513142752_initial_8.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendService.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavouriteProducts",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavouriteProducts", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FavouriteStores",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StoreName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavouriteStores", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavouriteProducts");
+
+            migrationBuilder.DropTable(
+                name: "FavouriteStores");
+        }
+    }
+}
