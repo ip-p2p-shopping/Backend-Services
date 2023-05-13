c7ec34546443f22e4bc64f71d38c2293d1ef93e5
Stefan Alexandru Virna <stefanalexandru02@gmail.com>
2023-05-13 13:49:06 +0300

    fix model

---
+++ BackendService/Migrations/20230513104903_initial_7.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendService.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingInstances",
+                table: "ShoppingInstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "ShoppingInstances",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingInstances",
+                table: "ShoppingInstances",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingInstances",
+                table: "ShoppingInstances");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ShoppingInstances");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingInstances",
+                table: "ShoppingInstances",
+                column: "UserId");
+        }
+    }
+}
